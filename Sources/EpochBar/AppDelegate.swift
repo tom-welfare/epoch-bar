85ff0fb1599c4d57f943d7061d3b231eb0f392a4
Tom Lansdall-Welfare <44580819+tom-welfare@users.noreply.github.com>
2026-04-22 15:38:50 +0100

    Prevent duplicate instances with a warning alert

--- Sources/EpochBar/AppDelegate.swift
+++ Sources/EpochBar/AppDelegate.swift
@@ -7,9 +7,26 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
 
     static func main() {
         let app = NSApplication.shared
+
+        if isAlreadyRunning() {
+            let alert = NSAlert()
+            alert.messageText = "EpochBar is already running"
+            alert.informativeText = "Another copy of EpochBar is already in the menu bar. This one will now quit."
+            alert.alertStyle = .warning
+            alert.runModal()
+            exit(0)
+        }
+
         let delegate = AppDelegate()
         app.delegate = delegate
         app.run()
+    }
+
+    private static func isAlreadyRunning() -> Bool {
+        guard let bundleID = Bundle.main.bundleIdentifier else { return false }
+        let ourPID = ProcessInfo.processInfo.processIdentifier
+        return NSRunningApplication.runningApplications(withBundleIdentifier: bundleID)
+            .contains { $0.processIdentifier != ourPID }
     }
 
     func applicationDidFinishLaunching(_ notification: Notification) {
